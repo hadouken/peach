674ef06cb8120da9f11c9b6d8d9c8d76de569a3b
Viktor Elofsson <viktor@viktorelofsson.se>
2013-11-12 17:52:16 +0100

    Added ReleasesController.

---
+++ src/Peach.WebApi/Controllers/ReleasesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Peach.WebApi.Controllers
+{
+    public class ReleasesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] {"1", "2"};
+        } 
+    }
+}
