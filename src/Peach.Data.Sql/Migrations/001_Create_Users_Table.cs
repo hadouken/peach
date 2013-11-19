9e82fd7f8498cff7df6017a6166ffa04fc5d3460
Viktor Elofsson <viktor@viktorelofsson.se>
2013-11-19 12:29:11 +0100

    Fixed migrations and added the first one.

---
+++ src/Peach.Data.Sql/Migrations/001_Create_Users_Table.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Peach.Data.Sql.Migrations
+{
+    [Migration(20131119122145)]
+    public class CreateUsersTable001 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey()
+                .WithColumn("ClaimedIdentifier").AsString(400).NotNullable().Unique()
+                .WithColumn("UserName").AsString(100).NotNullable().Unique();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Users");
+        }
+    }
+}
--- src/Peach.Data.Sql/SqlSchemaMigrator.cs
+++ src/Peach.Data.Sql/SqlSchemaMigrator.cs
@@ -1,9 +1,42 @@
-﻿namespace Peach.Data.Sql
+﻿using System.Configuration;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.SqlServer;
+using NHibernate.Linq;
+
+namespace Peach.Data.Sql
 {
     class SqlSchemaMigrator : ISchemaMigrator
     {
+        private readonly string _connectionString;
+
+        public SqlSchemaMigrator()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["Hadouken"].ConnectionString;
+        }
+
         public void Migrate()
         {
+            var processor = GetProcessor();
+            var context = new RunnerContext(new NullAnnouncer());
+
+            var migrationRunner = new MigrationRunner(
+                typeof (SqlSchemaMigrator).Assembly,
+                context,
+                processor);
+
+            migrationRunner.MigrateUp();
+        }
+
+        private IMigrationProcessor GetProcessor()
+        {
+            var factory = new SqlServerProcessorFactory();
+            var processor = factory.Create(_connectionString, new NullAnnouncer(), new ProcessorOptions());
+
+            return processor;
         }
     }
 }
