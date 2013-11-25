589c3c17cb019c111043a8d4fa8bb51e90c7f729
Viktor Elofsson <viktor@viktorelofsson.se>
2013-11-25 15:19:52 +0100

    Features view.

---
+++ src/Peach.Data/Domain/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Peach.Data.Domain
+{
+    public class Feature
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual string Slug { get; set; }
+
+        public virtual Uri ImageUri { get; set; }
+
+        public virtual string TagLine { get; set; }
+
+        public virtual string ShortContent { get; set; }
+
+        public virtual string Content { get; set; }
+    }
+}
