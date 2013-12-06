b1bcd8edd140d038ac0386b9575d7956dfae2480
Viktor Elofsson <viktor@viktorelofsson.se>
2013-12-06 16:17:38 +0100

    Logging

---
+++ src/Peach.Core/Diagnostics/LoggingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Peach.Core.Diagnostics
+{
+    public static class LoggingExtensions
+    {
+        public static void Debug(this ILogger logger, string format, params object[] args)
+        {
+            logger.Write(LogLevel.Debug, String.Format(format, args));
+        }
+
+        public static void DebugException(this ILogger logger, string message, Exception exception)
+        {
+            logger.Write(LogLevel.Debug, message, exception);
+        }
+    }
+}
