6449b2003003fbddf93c63ce69cb60a457d23730
Viktor Elofsson <viktor@viktorelofsson.se>
2014-01-26 02:13:00 +0100

    Web work

--- src/Peach.Data/Domain/Plugin.cs
+++ src/Peach.Data/Domain/Plugin.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Peach.Data.Domain
 {
     public class Plugin
     {
+        public virtual int Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Slug { get; set; }
+
+        public virtual User Author { get; set; }
+
+        public virtual Uri Homepage { get; set; }
+
+        public virtual string Description { get; set; }
     }
 }
