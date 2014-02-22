5ae785b63966c16433c5a5b2e848c094098f4398
Viktor Elofsson <viktor@viktorelofsson.se>
2014-02-22 14:25:17 +0100

    Added a WebApi controller to get the core plugins.

---
+++ src/Web/Peach.WebApi/Controllers/GetCoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Peach.Data;
+
+namespace Peach.WebApi.Controllers
+{
+    public class GetCoreController : ApiController
+    {
+        private readonly IPluginRepository _pluginRepository;
+
+        public GetCoreController(IPluginRepository pluginRepository)
+        {
+            _pluginRepository = pluginRepository;
+        }
+
+        [Route("get-core")]
+        public IEnumerable<Uri> Get()
+        {
+            var corePlugins = _pluginRepository.GetAll().Where(p => p.Name.StartsWith("core."));
+
+            return (from plugin in corePlugins
+                let latestRelease = plugin.Releases.OrderByDescending(r => r.ReleaseDate).FirstOrDefault()
+                where latestRelease != null
+                select latestRelease.DownloadUri);
+        }
+    }
+}
