4176e20692752caa4cafb697a8aaa9ccec3fd9d7
Viktor Elofsson <viktor@viktorelofsson.se>
2014-06-13 00:04:44 +0200

    Began updating the landing page with quick start.

---
+++ src/Web/Peach.Web/Controllers/InstallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Peach.Web.Controllers
+{
+    public class InstallController : Controller
+    {
+        //
+        [Route("install.ps1")]
+        public string Index()
+        {
+            return "powershell";
+        }
+	}
+}
