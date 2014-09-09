32fdc801283f12af90c5c58f2df47825cbfaac7c
Viktor Elofsson <viktor@viktorelofsson.se>
2014-09-09 15:11:57 +0200

    Added feature page.

---
+++ src/Web/Peach.Web/Controllers/FeaturesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Peach.Core;
+using Peach.Data;
+
+namespace Peach.Web.Controllers
+{
+    public sealed class FeaturesController : PeachController
+    {
+        public FeaturesController(IConfiguration configuration, IUserRepository userRepository)
+            : base(configuration, userRepository)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
